c93d34bea002f55ca3cb7efcdbd7d8411f1903dd
Sandesh Pathak <sandeshpathak0112@gmail.com>
2017-09-06 12:23:49 -0400

    create posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-	posts: PostsReducer
+	posts: PostsReducer,
+	// key should be form
+	form: formReducer
 });
 
 export default rootReducer;
